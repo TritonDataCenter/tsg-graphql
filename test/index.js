5e9ebcc1af5a03ff3665ab2f96a918756a5342b5
geek <wpreul@gmail.com>
2018-04-26 10:52:35 -0500

    Use latest graphi

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@
 
 const Path = require('path');
 // const { expect } = require('code');
+const Graphi = require('graphi');
 const Hapi = require('hapi');
 const Lab = require('lab');
 const TsgGql = require('../lib/');
@@ -10,14 +11,19 @@ const lab = exports.lab = Lab.script();
 const it = lab.it;
 
 
-const register = {
-  plugin: TsgGql,
-  options: {
-    keyPath: Path.join(__dirname, 'test.key'),
-    keyId: 'test',
-    apiBaseUrl: 'http://localhost'
+const register = [
+  {
+    plugin: Graphi
+  },
+  {
+    plugin: TsgGql,
+    options: {
+      keyPath: Path.join(__dirname, 'test.key'),
+      keyId: 'test',
+      apiBaseUrl: 'http://localhost'
+    }
   }
-};
+];
 
 it('can be registered with hapi', async () => {
   const server = new Hapi.Server();
